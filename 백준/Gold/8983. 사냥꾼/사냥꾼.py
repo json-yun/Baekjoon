90f4b8e2bfad498f23f238fd3193fe1d99eb6bd4
json-yun <jsyun833@gmail.com>
2024-07-06 23:22:56 +0900

    [Gold IV] Title: 사냥꾼, Time: 412 ms, Memory: 144940 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8983. 사냥꾼/사냥꾼.py
+++ 백준/Gold/8983. 사냥꾼/사냥꾼.py
@@ -1,32 +1,35 @@
 import sys
+from collections import defaultdict
 
 def main():
+    def bin_search(l, r, start, end):
+        if start >= end:
+            return False
+        mid = (end+start)//2
+        if LANE[mid] < l:
+            return bin_search(l, r, mid+1, end)
+        elif r < LANE[mid]:
+            return bin_search(l, r, start, mid)
+        else:
+            return True
+
     M, N, L = [int(i) for i in sys.stdin.readline().split()]
     LANE = [int(i) for i in sys.stdin.readline().split()]
     ANIMALS = [tuple(map(int, sys.stdin.readline().split())) for i in range(N)]
 
     MIN_X, MAX_X = [max(0, min(LANE)-L), min(1_000_000_000, max(LANE)+L)]
     MAX_Y = L
-    ANIMALS = [(x-y, x+y) for x, y in ANIMALS if all((y<=MAX_Y, MIN_X<=x<=MAX_X))]
-
+    animals = defaultdict(list)
+    for x, y in ANIMALS:
+        if all((y<=MAX_Y, MIN_X<=x<=MAX_X)):
+            animals[y].append(x)
     LANE.sort()
-    ANIMALS.sort()
 
     answer = 0
-    for h in LANE:
-        lb, rb = h-L, h+L
-        checked = []
-        for i, a in enumerate(ANIMALS):
-            if lb <= a[0]:
-                if a[1] <= rb:
-                    answer += 1
-                    checked.append(i)
-                elif rb < a[0]:
-                    break
-            else:
-                checked.append(i)
-        for c in sorted(checked, reverse=True):
-            ANIMALS.pop(c)
+    for y, xs in animals.items():
+        for x in xs:
+            if bin_search(x-(L-y), x+(L-y), 0, M):
+                answer += 1
     print(answer)
 
 main()
