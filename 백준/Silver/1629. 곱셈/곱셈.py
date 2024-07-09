1a86abee89cabc74e99269c7cea44cd9b25aa32b
json-yun <jsyun833@gmail.com>
2024-07-09 15:29:29 +0900

    [Silver I] Title: 곱셈, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -3,7 +3,13 @@ import sys
 A, B, C = [int(a) for a in sys.stdin.readline().split()]
 
 def main() -> None:
-    def _find_remainder(A, B, C):
+    # A = A * B + r (0 <= r < B, r은 자연수)
+    # A = x * y (x, y는 자연수) 일 때
+    # A = (x * B + r_x) * (y * B + r_y)
+    # A = (x * y * B * B) + (x * r_y * B) + (y * r_x * B) + (r_x * r_y)
+    # A = (xyB+xr_y+yr_x)*B + r_x*r_y 이므로
+    # r_x * r_y = z * B + r 을 만족하는 자연수 z가 존재한다.
+    def _find_remainder(A :int, B :int, C :int) -> int:
         B_half = B//2
         if B_half in cache:
             left = cache[B_half]
@@ -14,6 +20,7 @@ def main() -> None:
         else:
             right = _find_remainder(A, B-B_half, C)
         quotient = (left*right)%C
+        cache[B] = quotient
         return quotient
     cache = {0: 1,
              1: A%C}
