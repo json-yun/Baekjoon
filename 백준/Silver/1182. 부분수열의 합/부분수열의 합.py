795db62eec19455cc1513102252ceaad1dcd1e5b
json-yun <jsyun833@gmail.com>
2024-07-11 10:56:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 172 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -3,15 +3,17 @@ import sys
 N, S = [int(i) for i in sys.stdin.readline().split()]
 A = [int(i) for i in sys.stdin.readline().split()]
 
-def find_sum(level: int, total: int) -> int:
+def find_sum(index: int, total: int) -> int:
     count = 0
-    if level < N-1:
-        count += find_sum(level+1, total)
-    total += A[level]
+    # 현재 index의 값을 더하지 않고 진행한 경우
+    if index < N-1:
+        count += find_sum(index+1, total)
+    # 현재 index의 값을 더하고 진행한 경우
+    total += A[index]
     if total == S:
         count += 1
-    if level < N-1:
-        count += find_sum(level+1, total)
+    if index < N-1:
+        count += find_sum(index+1, total)
     return count
 
 print(find_sum(0, 0))
