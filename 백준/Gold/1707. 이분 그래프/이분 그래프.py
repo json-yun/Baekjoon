10b37a62bd157084cc3d4477a4d4833380fa90a0
json-yun <jsyun833@gmail.com>
2024-07-15 00:24:39 +0900

    [Gold IV] Title: 이분 그래프, Time: 7324 ms, Memory: 126336 KB -BaekjoonHub

---
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -0,0 +1,45 @@
+import sys
+from queue import Queue
+
+input = sys.stdin.readline
+
+class Vertex:
+    def __init__(self, key: int) -> None:
+        self.key = key
+        self.value = None
+        self.conn = [] # connected to _
+
+
+def main() -> None:
+    def bfs(n: int) -> bool:
+        all_vertexes = set([i for i in range(1, n+1)])
+        visited = set()
+        q = Queue()
+        while len(visited) < n:
+            start = (all_vertexes - visited).pop()
+            q.put(start)
+            vertexes[start].value = 0
+            while not q.empty():
+                v = q.get()
+                visited.add(v)
+                for i in vertexes[v].conn:
+                    if i not in visited:
+                        q.put(i)
+                        if vertexes[i].value is None:
+                            vertexes[i].value = (vertexes[v].value + 1) % 2
+                        elif vertexes[i].value == vertexes[v].value:
+                            return False
+        return True
+    
+    N = int(input())
+    for _ in range(N):
+        V, E  = map(int, input().split())
+        A = [list(map(int, input().split())) for _ in range(E)]
+        vertexes = {i: Vertex(i) for i in range(1, V+1)}
+        for a, b in A:
+            vertexes[a].conn.append(b)
+            vertexes[b].conn.append(a)
+        
+        print("YES" if bfs(V) else "NO")
+        
+main()
