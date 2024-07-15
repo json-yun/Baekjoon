55a095a97378e1ce5868659ce497b9708037bafa
json-yun <jsyun833@gmail.com>
2024-07-15 10:32:13 +0900

    [Gold IV] Title: 이분 그래프, Time: 1664 ms, Memory: 96588 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -7,39 +7,40 @@ class Vertex:
     def __init__(self, key: int) -> None:
         self.key = key
         self.value = None
-        self.conn = [] # connected to _
-
+        self.conn = []
 
 def main() -> None:
     def bfs(n: int) -> bool:
-        all_vertexes = set([i for i in range(1, n+1)])
         visited = set()
         q = deque()
-        while len(visited) < n:
-            start = (all_vertexes - visited).pop()
-            q.append(start)
-            vertexes[start].value = 0
-            while len(q):
-                v = q.popleft()
-                visited.add(v)
-                for i in vertexes[v].conn:
-                    if i not in visited:
-                        q.append(i)
+        
+        for start in range(1, n + 1):
+            if start not in visited:
+                q.append(start)
+                vertexes[start].value = 0
+                while q:
+                    v = q.popleft()
+                    if v in visited:
+                        continue
+                    visited.add(v)
+                    for i in vertexes[v].conn:
                         if vertexes[i].value is None:
                             vertexes[i].value = (vertexes[v].value + 1) % 2
+                            q.append(i)
                         elif vertexes[i].value == vertexes[v].value:
                             return False
         return True
-    
+
     N = int(input())
     for _ in range(N):
-        V, E  = map(int, input().split())
-        A = [list(map(int, input().split())) for _ in range(E)]
-        vertexes = {i: Vertex(i) for i in range(1, V+1)}
+        V, E = map(int, input().split())
+        A = [tuple(map(int, input().split())) for _ in range(E)]
+        vertexes = {i: Vertex(i) for i in range(1, V + 1)}
         for a, b in A:
             vertexes[a].conn.append(b)
             vertexes[b].conn.append(a)
-        
+
         print("YES" if bfs(V) else "NO")
-        
-main()
+
+if __name__ == "__main__":
+    main()
