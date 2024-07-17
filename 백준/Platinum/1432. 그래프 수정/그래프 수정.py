5e3af5fff641175a7f88aa2d31c6ba80ab48a60d
json-yun <jsyun833@gmail.com>
2024-07-17 14:01:24 +0900

    [Platinum IV] Title: 그래프 수정, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/1432. 그래프 수정/그래프 수정.py
+++ 백준/Platinum/1432. 그래프 수정/그래프 수정.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
@@ -11,22 +10,17 @@ class Vertex:
 def main() -> None:
     def find_no_outbound(include: list) -> list:
         for i in include:
-            flag = True
-            for j in include:
-                if vertexes[i].adj_vector[j-1]:
-                    flag = False
-                    break
-            if flag:
+            if all(not vertexes[i].adj_vector[j-1] for j in include):
                 include.remove(i)
                 return i
         return None
-    N = int(input())
-    vertexes: dict[int, Vertex] = {i: Vertex(i) for i in range(1, N+1)}
     # 데이터 불러오기
+    N = int(input())
+    vertexes: dict[int, Vertex] = {i: Vertex(i) for i in range(N, 0, -1)}
     for i in range(1, N+1):
         vertexes[i].adj_vector = list(map(int, list(input().rstrip())))
     
-    waiting = list(reversed(vertexes.keys()))
+    waiting = list(vertexes.keys())
     n = N
     while waiting:
         next = find_no_outbound(waiting)
@@ -35,14 +29,6 @@ def main() -> None:
             return
         vertexes[next].key = n
         n -= 1
-    # flag = True
-    # for i in range(1, N+1):
-    #     if vertexes[i].key != i:
-    #         flag = False
-    #         break
-    # if flag:
-    #     print(-1)
-    #     return
     print(*[vertexes[i].key for i in range(1, N+1)])
     
 if __name__ == "__main__":
