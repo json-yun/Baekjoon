2458090dc9aaedd1d038de5e28a6322328fe3429
json-yun <jsyun833@gmail.com>
2024-07-23 00:43:26 +0900

    [Silver III] Title: 01타일, Time: 416 ms, Memory: 39060 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -3,13 +3,18 @@ import sys
 input = sys.stdin.readline
 
 def main() -> None:
-    def tiles(n_left: int) -> int:
-        result = [1, 1]
-        for i in range(n_left-1):
-            if (result[(i+1)%2], result[i%2]) not in cache:
-                cache[(result[(i+1)%2], result[i%2])] = sum(result)%15746
-            result[i%2] = cache[(result[(i+1)%2], result[i%2])]
-        return result[n_left%2]
+    def tiles(n: int) -> int:
+        r = [1, 1]
+        
+        for i in range(n-1):
+            a, b = (i+1)%2, i%2
+            
+            if (r[a], r[b]) not in cache:
+                cache[(r[a], r[b])] = sum(r)%15746
+            r[b] = cache[(r[a], r[b])]
+            
+        return r[n%2]
+    
     cache = {}
     print(tiles(int(input())))
 
