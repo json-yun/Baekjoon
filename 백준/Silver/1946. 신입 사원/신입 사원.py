59c662ae612bc99a27c05b7bd60674c9d09a18d0
json-yun <jsyun833@gmail.com>
2024-07-24 15:15:58 +0900

    [Silver I] Title: 신입 사원, Time: 3028 ms, Memory: 58536 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,23 +1,23 @@
 import sys
+
 input = sys.stdin.readline
+
 def main() -> None:
     CASES = int(input())
     
     for _ in range(CASES):
         N = int(input()) # 지원자
-        P = [list(map(int, input().split())) for _ in range(N)]
+        P = [tuple(map(int, input().split())) for _ in range(N)]
         P.sort()
 
         m = 100001
-        result = 0
-        for i in range(N):
-            if P[i][1] < m:
-                m = P[i][1]
-                result += 1
-            elif m == 1:
-                break
+        for _, p in P:
+            if p < m:
+                m = p
+            else:
+                N -= 1
         
-        print(result)
+        print(N)
 
 if __name__ == "__main__":
     main()
