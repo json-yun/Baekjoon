6b7d7c701166a36b45c0223c6d6095897d350e4c
json-yun <jsyun833@gmail.com>
2024-08-10 12:06:52 +0900

    [level 3] Title: 가장 먼 노드, Time: 44.84 ms, Memory: 21.5 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,30 +1,3 @@
-from collections import deque
-
-
 def solution(n, edge):
     answer = 0
-    visited = [0 for _ in range(n+1)]
-    visited[1] = 1
-    edges = {i: [] for i in range(1, n+1)}
-    for i, j in edge:
-        edges[i].append(j)
-        edges[j].append(i)
-        
-    q = deque()
-    q.append((1, 0))
-    steps = 0
-    max_d = 0
-    while len(q):
-        cur, steps = q.popleft()
-        if steps > max_d:
-            answer = 1
-            max_d = steps
-        elif steps == max_d:
-            answer += 1
-        
-        for e in edges[cur]:
-            if not visited[e]:
-                visited[e] = 1
-                q.append((e, steps+1))
-        
     return answer
