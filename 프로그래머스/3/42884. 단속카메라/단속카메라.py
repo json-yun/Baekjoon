190e770fc05dfdbcfc2163198e078eeb23556e3a
json-yun <jsyun833@gmail.com>
2024-08-14 11:02:00 +0900

    [level 3] Title: 단속카메라, Time: 0.93 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,12 +1,11 @@
 def solution(routes):
     answer = 0
     routes.sort(key=lambda x: x[1])
+    
     last_cam = -30001
     for route in routes:
-        if not is_in(route, last_cam):
+        if not route[0] <= last_cam:
             last_cam = route[1]
             answer += 1
+            
     return answer
-
-def is_in(route, cam):
-    return (route[0] <= cam <= route[1])
