9e0d8d503a1446a1f4cca64d97fb0f1f7144b23c
json-yun <jsyun833@gmail.com>
2024-08-22 20:48:46 +0900

    [Gold V] Title: AC, Time: 100 ms, Memory: 41828 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.py
@@ -0,0 +1,31 @@
+def main() -> None:
+    TC = int(input())
+
+    for _ in range(TC):
+        INST = input().rstrip()
+        N = int(input())
+        ARR = [i for i in input().strip('[]').split(',')]
+
+        d_count = 0
+        inv = False
+        for inst in INST:
+            if inst == 'D':
+                N -= 1
+                if N < 0:
+                    break
+                if not inv:
+                    d_count += 1
+            else:
+                inv = not inv
+
+        if N >= 0:
+            ARR = ARR[d_count:d_count+N]
+            if inv:
+                ARR.reverse()
+            answer = '[' + ','.join(ARR) + ']'
+
+            print(answer)
+        else:
+            print("error")
+        
+main()
