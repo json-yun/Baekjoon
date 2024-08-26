62e5b088832203ee49fa4e04de104648af813b27
json-yun <jsyun833@gmail.com>
2024-08-26 14:29:07 +0900

    [Gold IV] Title: 문자열 폭발, Time: 220 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -8,27 +8,21 @@ def main():
 
     cnt = -1
     for s in STRING:
-        if s == C4[cnt+1]:
-            cnt += 1
-        elif s == C4[0]:
-            cnt = 0
-        else:
-            cnt = -1
-            
+        cnt += 1
+        if s != C4[cnt]:
+            cnt = 0 if s == C4[0] else -1
+        
         if cnt == L-1:
             for _ in range(L-1):
                 check_stack.pop()
                 cnt_stack.pop()
+                
             cnt = cnt_stack[-1] if cnt_stack else -1
         else:
             check_stack.append(s)
             cnt_stack.append(cnt)
         
     result = ''.join(check_stack)
-
-    if result == '':
-        print("FRULA")
-    else:
-        print(result)
+    print(result if result != '' else "FRULA")
 
 main()
