50d55e8726db3f347faae3f2578175eae7aab990
json-yun <jsyun833@gmail.com>
2024-08-28 13:41:00 +0900

    [Silver I] Title: 동물원, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,10 +1,11 @@
 def main():
     N = int(input())
 
-    oo = ol = 1
+    a_n = 3
+    a_n1 = 1 # n-2까지의 합
     for _ in range(N-1):
-        oo, ol = oo+ol+ol, oo+ol
-
-    print(sum([oo, ol*2])%9901)
+        a_n, a_n1 = (a_n + 2 + 2*a_n1)%9901, (a_n1+a_n)%9901
+        
+    print(a_n)
 
 main()
