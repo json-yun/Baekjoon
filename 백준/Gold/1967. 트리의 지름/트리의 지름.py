0335bdd911bee3139cd199cd746ec246d736dded
json-yun <jsyun833@gmail.com>
2024-09-02 13:28:16 +0900

    [Gold IV] Title: 트리의 지름, Time: 48 ms, Memory: 35212 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -6,36 +6,39 @@ input = sys.stdin.readline
 def main():
     N = int(input())
     childs = [{}] + [{} for _ in range(N)]
-    parents = [None] + [None for _ in range(N)]
+    parents = [0] + [0 for _ in range(N)]
     level = [0]*(N+1)
 
-    max_level = 0
+    tree_height = 0
     for i in range(1, N):
         p, c, w = map(int, input().split())
         childs[p][c] = w
         parents[c] = p
         level[c] = level[p] + 1
-        max_level = max_level if max_level >= level[c] else level[c]
+        tree_height = tree_height if tree_height >= level[c] else level[c]
 
-    level_dict = {i: [] for i in range(max_level+1)}
+    level_dict = [[] for _ in range(tree_height+1)]
     for i in range(1, N+1):
         level_dict[level[i]].append(i)
         
-    for q in reversed(level_dict.values()):
+    for q in reversed(level_dict):
         for i in q:
-            if parents[i] is None:
-                break
+            if len(childs[i]) > 2:
+                a = sorted(childs[i].items(), key=lambda x: -x[1])[:2]
+                childs[i] = dict(a)
                 
             max_w = 0
             for w in childs[i].values():
-                max_w = max(max_w, w)
-            childs[parents[i]][i] += max_w
+                if w > max_w:
+                    max_w = w
+
+            if i != 1:
+                childs[parents[i]][i] += max_w
 
     max_l = 0
     for v in childs:
-        vs = list(v.values())
-        vs.sort(reverse=True)
-        max_l = max(max_l, sum(vs[:2]))
+        if (v:=sum(v.values())) > max_l:
+            max_l = v
             
     print(max_l)
 
