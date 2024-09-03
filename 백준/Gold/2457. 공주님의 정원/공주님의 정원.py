dded27c5f7d470336fb9b2418db3fcaa2cb91b52
json-yun <jsyun833@gmail.com>
2024-09-03 16:16:38 +0900

    [Gold III] Title: 공주님의 정원, Time: 164 ms, Memory: 42044 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -12,14 +12,14 @@ def main():
     for _ in range(N):
         m1, d1, m2, d2 = map(int, input().split())
         flowers.append((m_to_d[m1]+d1, m_to_d[m2]+d2))
-    flowers.sort()
+    flowers.sort(key=lambda x: x[0])
 
     curr = m_to_d[3]+1 # 3월 1일 시작
     temp = 0
     cnt = 0
     i = 0
     while curr < m_to_d[12]+1: # 12월 1일 이후로 지는 꽃이 있어야 함
-        if i < len(flowers) and flowers[i][0] <= curr:
+        if i < N and flowers[i][0] <= curr:
             temp = max(temp, flowers[i][1])
             i += 1
         else:
