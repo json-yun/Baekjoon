f119fa71cf2cbee91e5d5d000c889d5fd5d574c1
json-yun <jsyun833@gmail.com>
2024-09-03 13:10:43 +0900

    [Gold III] Title: 공주님의 정원, Time: 216 ms, Memory: 40484 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -4,34 +4,32 @@ input = sys.stdin.readline
 
 def main():
     N = int(input())
-    month_to_date = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    month_to_date = [sum(month_to_date[:i]) for i in range(13)]
+    m_to_d = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    m_to_d = [sum(m_to_d[:i]) for i in range(13)]
 
     flowers = []
     for _ in range(N):
         m1, d1, m2, d2 = map(int, input().split())
-        flowers.append((month_to_date[m1]+d1, month_to_date[m2]+d2))
-    # 60-334
+        flowers.append((m_to_d[m1]+d1, m_to_d[m2]+d2))
 
-    flowers.sort(key=lambda x: (x[0], x[1]))
+    flowers.sort()
 
-    curr = 60
+    curr = m_to_d[3]+1
     temp = 0
     cnt = 0
     i = 0
-    while curr <= 334:
+    while curr < m_to_d[12]+1:
         if i < len(flowers) and flowers[i][0] <= curr:
-            if flowers[i][1] > temp:
-                temp = flowers[i][1]
+            temp = max(temp, flowers[i][1])
             i += 1
         else:
-            if temp:
-                curr = temp
-                temp = 0
-                cnt += 1
-            else:
+            if temp == 0:
                 cnt = 0
                 break
+            
+            curr = temp
+            temp = 0
+            cnt += 1
 
     print(cnt)
 
