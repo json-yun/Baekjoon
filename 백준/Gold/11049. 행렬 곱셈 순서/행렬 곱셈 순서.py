e7415dd3ad9b872f0b0500a8229bcd169f11ea78
json-yun <jsyun833@gmail.com>
2024-09-23 15:44:11 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 640 ms, Memory: 112360 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,16 +1,20 @@
 import sys
 input = sys.stdin.readline
 INF = 2**31
+
 def main() -> None:
     N = int(input())
     A = [tuple(map(int, input().split())) for _ in range(N)]
     c = [[INF if i != j else 0 for j in range(N)] for i in range(N)]
+    
     for i in range(2, N+1): # 길이-1
         for j in range(0, N-i+1): # 위치
             for k in range(1, i):
-                if c[j][j+i-1] > (m:=c[j][j+k-1] + c[j+k][j+i-1] + A[j][0]*A[j+k][0]*A[j+i-1][-1]):
-                    c[j][j+i-1] = m
-    # print(*c, sep="\n")
+                s, m, e = j, j+k-1, j+i-1
+                if c[s][e] > (m:=c[s][m] + c[m+1][e] + A[j][0]*A[m][1]*A[e][1]):
+                    c[s][e] = m
+                    
     print(c[0][N-1])
-    
-main()
+
+if __name__ == "__main__":
+    main()
