29a8a13d40b5de20b6b809d0f6140050d985168c
json-yun <jsyun833@gmail.com>
2024-09-30 13:30:28 +0900

    [Gold V] Title: 개똥벌레, Time: 272 ms, Memory: 49048 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.py
@@ -0,0 +1,31 @@
+import sys
+import test
+input = sys.stdin.readline
+
+def main() -> None:
+    N, H = map(int, input().split())
+    
+    cone, icicle = {}, {}
+    for i in range(N):
+        if i % 2 == 0:
+            h = int(input())
+            cone[h] = cone.get(h, 0) + 1
+        else:
+            h = int(input())
+            icicle[H-h] = icicle.get(H-h, 0) + 1
+
+    total = N//2
+    min_wall = N//2
+    count = 0
+    for i in range(H):
+        total -= cone.get(i, 0) - icicle.get(i, 0)
+        if total < min_wall:
+            min_wall = total
+            count = 1
+        elif total == min_wall:
+            count += 1
+
+    print(min_wall, count)
+    
+if __name__ == "__main__":
+    sys.exit(main())
