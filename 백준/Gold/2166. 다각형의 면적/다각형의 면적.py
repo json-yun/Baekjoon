b35d2fa6e56571415e171ed36e9f24f9403b19ca
json-yun <jsyun833@gmail.com>
2024-11-29 15:55:21 +0900

    [Gold V] Title: 다각형의 면적, Time: 292 ms, Memory: 32140 KB -BaekjoonHub

---
+++ 백준/Gold/2166. 다각형의 면적/다각형의 면적.py
@@ -0,0 +1,17 @@
+def cross_product(vectors: list[tuple]) -> float:
+    inner = 0
+    outer = 0
+    x_0, y_0 = vectors[-1]
+    for x, y in vectors:
+        inner += x_0 * y
+        outer += y_0 * x
+        x_0 = x
+        y_0 = y
+
+    return abs(inner-outer)/2
+
+N = int(input())
+poly = [tuple(map(int, input().split())) for _ in range(N)]
+area = cross_product(poly)
+
+print(f"{area:.1f}")
