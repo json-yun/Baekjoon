071f5c6c77ca36ed0219e58bc816e4f67a47a0bc
json-yun <jsyun833@gmail.com>
2025-02-04 22:14:46 +0900

    [Unrated] Title: 0.1, Time: 96 ms, Memory: 32412 KB, Score: 351104 point -BaekjoonHub

--- 백준/Unrated/11921. 0．1/0．1.py
+++ 백준/Unrated/11921. 0．1/0．1.py
@@ -6,21 +6,19 @@ input = sys.stdin.readline
 print = sys.stdout.write
 
 def main():
-    s = time.time()
+    s = time.perf_counter()
     n = 0
     total = 0
-    fd = 0
-    byte = 10000
-    a, _, b = os.read(fd, byte).decode('utf-8').rpartition('\n')
+    a, _, b = os.read(0, 10000).decode('utf-8').rpartition('\n')
     list_a = a.split()
     i = len(list_a) - 1
     m = map(int, list_a)
     m.__next__()
     total += sum(m)
 
-    while time.time() < s + 0.07:
+    while time.perf_counter() < s + 0.09:
         temp = b
-        a, _, b = os.read(fd, byte).decode('utf-8').rpartition('\n')
+        a, _, b = os.read(0, 10000).decode('utf-8').rpartition('\n')
         a = temp + a
         list_a = a.split()
         i += len(list_a)
