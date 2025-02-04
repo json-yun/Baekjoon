2029b88565983d027476f330331ec1fadd34a60d
json-yun <jsyun833@gmail.com>
2025-02-04 21:52:08 +0900

    [Unrated] Title: 0.1, Time: 80 ms, Memory: 32412 KB, Score: 271231 point -BaekjoonHub

--- 백준/Unrated/11921. 0．1/0．1.py
+++ 백준/Unrated/11921. 0．1/0．1.py
@@ -13,23 +13,19 @@ def main():
     byte = 10000
     a, _, b = os.read(fd, byte).decode('utf-8').rpartition('\n')
     list_a = a.split()
+    i = len(list_a) - 1
     m = map(int, list_a)
     m.__next__()
-    for i, n in enumerate(m, 1):
-        total += n
-        if time.time() > s + 0.9:
-            break
+    total += sum(m)
 
     while time.time() < s + 0.07:
         temp = b
         a, _, b = os.read(fd, byte).decode('utf-8').rpartition('\n')
         a = temp + a
         list_a = a.split()
+        i += len(list_a)
         m = map(int, list_a)
-        for i, n in enumerate(m, i+1):
-            total += n
-            if time.time() > s + 0.9:
-                break
+        total += sum(m)
 
     print(str(i) + '\n')
     print(str(total))
